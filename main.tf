8352b9ef580ea625d0d8ff079378e3afa1ecd039
karthikeyan <rakarthik@yahoo.com>
2023-12-26 16:03:19 +1100

    added vars

--- main.tf
+++ main.tf
@@ -7,9 +7,20 @@ terraform {
   }
 }
 
+variable "aws_access_key_id" {
+  type = string
+}
+
+variable "aws_secret_access_key" {
+  type = string
+}
+
 provider "aws" {
   region = "ap-southeast-2"
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
 }
+
 
 resource "aws_instance" "web1" {
     availability_zone = "ap-southeast-2b"
